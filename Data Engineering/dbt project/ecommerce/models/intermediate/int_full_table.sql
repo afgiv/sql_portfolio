e4ed16bf150623eff78f6fc0084cc788b53e8498
afgiv <amadeofgiv@gmail.com>
2025-10-06 01:02:22 +0800

    Finish model for core table

--- Data Engineering/dbt project/ecommerce/models/intermediate/int_full_table.sql
+++ Data Engineering/dbt project/ecommerce/models/intermediate/int_full_table.sql
@@ -1,55 +1,30 @@
-WITH order_items AS (
+WITH orders AS (
     SELECT *
-    FROM {{ ref('stg_order_items') }}
-), orders AS (
-    SELECT or.*, o.date_purchased, o.date_processed, o.date_shipped,
-    o.expected_delivery_date, o.date_delivered
-    FROM order_items AS or
-    JOIN {{ ref('stg_orders') }} AS o
-        ON or.order_id = o.order_id
+    FROM {{ ref('int_orders') }}
 ), products AS (
-    SELECT o.*, p.category_name, p.title_length, p.description_length,
-    p.photos_qty, p.weight_g, p.length_cm, p.height_cm, p.width_cm
+    SELECT o.*, p.category_name, p.category_translation, p.title_length,
+    p.description_length, p.photos_qty, p.weight_g, p.length_cm, p.height_cm,
+    p.width_cm
     FROM orders AS o
-    JOIN {{ ref('stg_products') }} AS p
+    LEFT JOIN {{ ref('int_products') }} AS p
         ON o.product_id = p.product_id
-), product_category AS (
-    SELECT p.*, pc.category_translation
-    FROM products AS p
-    JOIN {{ ref('stg_product_category') }} AS pc
-        ON p.category_name = pc.category_name
-), sellers AS (
-    SELECT pc.*, s.zip_code_prefix, s.city AS seller_city, s.state
-    AS seller_state
-    FROM product_category AS pc
-    JOIN {{ ref('stg_sellers') }} AS s
-        ON pc.seller_id = s.seller_id
-), geolocation AS (
-    SELECT s.*, g.latitude, g.longitude, g.city, g.state
-    FROM sellers AS s
-    JOIN {{ ref('stg_geolocation') }} AS g
-        ON s.zip_code_prefix = g.zip_code_prefix
 ), customers AS (
-    SELECT g.*, c.customer_unique_id, c.city AS customer_city,
-    c.state AS customer_state
-    FROM geolocation AS g
-    JOIN {{ ref('stg_customers') }} AS c
-        ON g.customer_id = c.customer_id
-), order_payments AS (
-    SELECT c.*, op.attemps_made, op.payment_type, op.payment_installments,
-    op.total_payment
+    SELECT p.*, c.customer_unique_id, c.zip_code_prefix AS customer_zip_code,
+    c.city AS customer_city, c.state AS customer_state, c.customer_lat, c.customer_lng,
+    c.customer_city AS c.geo_c_city, c.state AS geo_c_state
+    FROM products AS p
+    LEFT JOIN {{ ref('int_customers') }} AS c
+        ON p.customer_id = c.customer_id
+), sellers AS (
+    SELECT c.*, s.zip_code_prefix AS seller_zip_code, s.seller_lat, s.seller_lng,
+    s.city AS seller_city, s.state AS seller_state, s.seller_city AS geo_s_city,
+    s.seller_state AS geo_s_state
     FROM customers AS c
-    JOIN {{ ref('stg_order_payments') }} AS op
-        ON c.order_id = op.order_id
-), order_reviews AS (
-    SELECT op.*, r.review_id, r.score, r.title, r.comment, r.date_submitted,
-    r.date_processed
-    FROM order_payments AS op
-    JOIN {{ ref('stg_order_reviews') }} AS r
-        ON op.order_id = r.order_id
+    LEFT JOIN {{ ref('int_sellers') }} AS  s
+        ON c.seller_id = s.seller_id
 ), final AS (
     SELECT *
-    FROM order_reviews
+    FROM sellers
 )
 
 SELECT * FROM final
