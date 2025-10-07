db5b2e4a0d3effa45432c366a1e94da59bd37cab
afgiv <amadeofgiv@gmail.com>
2025-10-07 19:47:09 +0800

    Profile the data

---
+++ Data Engineering/dbt project/postgresql/data_profiling.sql
@@ -0,0 +1,60 @@
+SELECT * FROM staging_to_intermediate.int_full_table;
+
+SELECT COUNT(*) FROM staging_to_intermediate.int_full_table;
+-- Total Row Count: 112250
+
+-- Check relationship between cities and states for customer, seller, and geolocation
+SELECT customer_city, geo_c_city, customer_state, geo_c_city,
+seller_city, geo_s_city, seller_state, geo_s_state
+FROM staging_to_intermediate.int_full_table
+-- Only shows redundancy, can be removed in the dims
+
+-- Grain check
+SELECT COUNT(*) AS total_rows, COUNT(DISTINCT order_id) AS grain_count
+FROM staging_to_intermediate.int_full_table;
+-- 112250 vs 98312
+SELECT COUNT(*) AS total_rows, COUNT(DISTINCT (order_id, order_item_num)) AS grain_count
+FROM staging_to_intermediate.int_full_table;
+-- 112250 vs 112250
+-- Result: Matching row count
+-- Action: order_id + order_item_num are the composite keys for the fact table
+
+-- Cardinality check
+
+-- Fact table
+SELECT COUNT(*) AS total_rows, COUNT(DISTINCT product_id) AS unique_values,
+ROUND((CAST(COUNT(DISTINCT product_id) AS NUMERIC)/COUNT(*)) * 100, 2) AS unique_pct
+FROM staging_to_intermediate.int_full_table;
+-- Result: 29.35% of uniqueness
+-- Action: product_id is a dimension key
+
+SELECT COUNT(*) AS total_rows, COUNT(DISTINCT seller_id) AS unique_values,
+ROUND((CAST(COUNT(DISTINCT seller_id) AS NUMERIC)/COUNT(*)) * 100, 2) AS unique_pct
+FROM staging_to_intermediate.int_full_table;
+-- Result: 2.76% of uniqueness
+-- Action: seller_id is a dimension key (seller_id is an entity)
+
+SELECT COUNT(*) AS total_rows, COUNT(DISTINCT expected_shipping_date) AS unique_values,
+ROUND((CAST(COUNT(DISTINCT expected_shipping_date) AS NUMERIC)/COUNT(*)) * 100, 2) AS unique_pct
+FROM staging_to_intermediate.int_full_table;
+-- Result: 82.89% of uniqueness
+-- Action: expected_shipping_date is a potential dimension key for another table
+
+-- Customer dimension
+SELECT COUNT(*) AS total_rows, COUNT(DISTINCT customer_id) AS unique_values,
+ROUND((CAST(COUNT(DISTINCT customer_id) AS NUMERIC)/COUNT(*)) * 100, 2) AS unique_pct
+FROM staging_to_intermediate.int_full_table;
+-- Result: 87.58%
+-- Action: customer_id is a potential dimension key
+
+SELECT COUNT(*) AS total_rows, COUNT(DISTINCT customer_unique_id) AS unique_values,
+ROUND((CAST(COUNT(DISTINCT customer_unique_id) AS NUMERIC)/COUNT(*)) * 100, 2) AS unique_pct
+FROM staging_to_intermediate.int_full_table;
+-- Result: 84.46%
+-- Action: customer_unique_id is a potential dimension key
+
+SELECT COUNT(*) AS total_rows, COUNT(DISTINCT customer_zip_code) AS unique_values,
+ROUND((CAST(COUNT(DISTINCT customer_zip_code) AS NUMERIC)/COUNT(*)) * 100, 2) AS unique_pct
+FROM staging_to_intermediate.int_full_table;
+-- Result: 13.19%
+-- Action: customer_zip_code is a potential dimension key
