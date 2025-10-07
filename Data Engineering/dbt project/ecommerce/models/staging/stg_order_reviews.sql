954e97adf559dd66f1fd913e54aa5bd2b819b223
afgiv <amadeofgiv@gmail.com>
2025-10-07 08:59:52 +0800

    Add unique test on stg_order_reviews; Edit stg_order_reviews query; Rename intermediate schema

--- Data Engineering/dbt project/ecommerce/models/staging/stg_order_reviews.sql
+++ Data Engineering/dbt project/ecommerce/models/staging/stg_order_reviews.sql
@@ -21,12 +21,26 @@
 WITH deduplicate AS (
     SELECT DISTINCT *
     FROM {{ source('raw', 'order_reviews') }}
+), max_review AS (
+    SELECT order_id, MAX(review_creation_date) AS review_creation_date,
+    AVG(review_score) AS review_score,
+    MAX(review_answer_timestamp) AS review_answer_timestamp
+    FROM deduplicate
+    GROUP BY order_id
+), full_table AS (
+    SELECT d.review_id, m.order_id, m.review_score, d.review_comment_title,
+    d.review_comment_message, m.review_creation_date, m.review_answer_timestamp
+    FROM deduplicate AS d
+    JOIN max_review AS m
+        ON d.order_id = m.order_id
+        AND d.review_creation_date = m.review_creation_date
+        AND d.review_answer_timestamp = m.review_answer_timestamp
 ),standardize AS (
     SELECT review_id, order_id, review_score AS score,
     INITCAP(review_comment_title) AS title, review_comment_message AS comment,
     review_creation_date AS date_submitted,
     review_answer_timestamp AS date_processed
-    FROM deduplicate
+    FROM full_table
 ), final AS (
     SELECT *
     FROM standardize
